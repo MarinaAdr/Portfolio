8766f03837143ec44c3639c58adc07ae32173be7
Claudia <marinaclaudia003@gmail.com>
2024-04-13 20:13:57 +0300

    ajout du dossier components et installation des icones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Nav from '@/Components/Nav';
 import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        <Nav/>
+      </div>
+    </div>
   )
 }
 
