11e38484fbd8c7e82d4161253f5bfdfce4b384a4
Claudia <marinaclaudia003@gmail.com>
2024-04-14 08:16:38 +0300

    rendre mobile le navbar

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,7 +1,11 @@
-import { Bars2Icon, Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-const Nav = () => {
+interface Props {
+  openNav:()=>void;
+}
+
+const Nav = ({openNav}:Props) => {
   return (
     <div className='w-[100%] top-0 h-[12vh] bg-[#0e0e31] shadow-md'>
         <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
@@ -13,7 +17,7 @@ const Nav = () => {
             <div className='nav-link'>ABOUT</div>
             <div className='nav-link'>PROJECTS</div>
             <div className='nav-link'>CONTACT</div>
-            <div>
+            <div onClick={openNav}>
                 <Bars3Icon className='w-[2rem] h-[2rem] md:hidden cursor-pointer text-pink-300'/>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Nav from '@/Components/Nav';
-import React from 'react';
+import React, { useState } from 'react';
+import MobileNav from '@/Components/MobileNav';
 
 const HomePage = () => {
+
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
   return (
     <div className='overflow-x-hidden'>
       <div>
-        <Nav/>
+        <MobileNav nav={nav} closeNav={closeNav}/>
+        <Nav openNav={openNav}/>
       </div>
     </div>
   )
